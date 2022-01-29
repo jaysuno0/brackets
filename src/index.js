c0fb4211665b85a060a76abf44eaaeea7eddd88e
Daniil Kochkin <jaysun0@yanex.ru>
2022-01-29 20:33:45 +0300

    feat: finish brackets-check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let resultString = str;
+  bracketTypes = bracketsConfig.map(type => type.join(''));
+
+  for (let i = 0; i < str.length / 2; i++) {
+    bracketTypes.forEach(bracket => resultString = resultString.replace(bracket, ''));
+  }
+
+  return resultString.length === 0;
 }
